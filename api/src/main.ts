f5e248cbf8ff4c531b7a2ee7d50c48f62c45e2ec
Mitch Chimwemwe chanza <mitch@opherlabs.co.za>
2023-11-13 06:55:16 +0200

    feat: update
    Signed-off-by: Mitch Chimwemwe chanza <mitch@opherlabs.co.za>

--- api/src/main.ts
+++ api/src/main.ts
@@ -9,23 +9,20 @@ dotenv.config();
 (async () => {
     const env = process.env;
     const app = express();
-    // Initialse base router
-    const router = express.Router();
-    // Set initial route
-    router.get("/", (_req, res) => {
-        res.send({ message: "Hello, World!" });
-    });
+
     // Disable powered by header 
     app.disable('x-powered-by');
-
-    // Set v1/api endpoint
-    app.use("/v1/rest", restEndpoint);
     // configure cors
     app.use(cors({ origin: env.ALLOWED_ORIGINS?.split(",") }));
     // enable json serialization
     app.use(json());
+
+    // Set v1/rest endpoint
+    app.use("/v1/rest", restEndpoint);
+
     // start apollo server
     await server.start()
+    // Set v1/graphql endpoint
     app.use('/v1/graphql', expressMiddleware(server));
     app.listen(env.PORT ? +env.PORT : 9000, () => {
         console.log({
